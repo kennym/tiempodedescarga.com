bc514e4cc171e9cbf639a4ebd256983707b32517
Kenny Meyer <kenny@kennymeyer.net>
2019-11-09 22:20:46 -0300

    Updates

---
+++ precache-manifest.a28a8adeb53becd793b3875a2fae3a35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43b0107e7b78d721c3380d2f511d0ec9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75b9224e8dd5812529f2",
+    "url": "/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "fadb1aa07fc2bad04957",
+    "url": "/static/css/main.f6041900.chunk.css"
+  },
+  {
+    "revision": "75b9224e8dd5812529f2",
+    "url": "/static/js/2.fbaa81f6.chunk.js"
+  },
+  {
+    "revision": "fadb1aa07fc2bad04957",
+    "url": "/static/js/main.bf4f2f54.chunk.js"
+  },
+  {
+    "revision": "89dd65885409c0949e6b",
+    "url": "/static/js/runtime-main.7511e111.js"
+  }
+]);
